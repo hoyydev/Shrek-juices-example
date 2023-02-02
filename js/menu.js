63c2e143ceffec6fff4f51c9fceaa27cfd6128e4
Tienanhmr <118037885+Tienanhmr@users.noreply.github.com>
2023-02-02 12:37:31 +0700

    đức ngu

--- js/menu.js
+++ js/menu.js
@@ -70,4 +70,17 @@ $(document).ready(function() {
 
     // }
 });
-
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('item');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
